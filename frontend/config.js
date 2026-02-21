782037422095f03b23f176f1eedde37427a411d4
Steve Aurora <steve2018aurora@gmail.com>
2026-02-21 16:45:28 +1100

    add automated Cognito login to Notes API tester

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,23 @@
 // frontend/config.js
 // Simple global config for the Notes API tester.
 
+// Your API Gateway base URL (NO trailing slash)
 window.NOTES_API_CONFIG = {
-  // Your API Gateway invoke URL (no trailing slash)
   apiBaseUrl: "https://ss5k5py93l.execute-api.ap-southeast-2.amazonaws.com/prod",
+};
+
+// Your Cognito Hosted UI config
+window.COGNITO_CONFIG = {
+  // Cognito domain (no trailing slash)
+  domain: "https://ap-southeast-2xlwv5qp6j.auth.ap-southeast-2.amazoncognito.com",
+
+  // App client ID
+  clientId: "567v4db047v4jpujij7me8nb0u",
+
+  // Where Cognito sends you back AFTER login
+  // (we'll host notes.html here eventually)
+  redirectUri: "https://d84l1y8p4kdic.cloudfront.net/notes.html",
+
+  // Scopes to request
+  scope: "email openid phone",
 };
